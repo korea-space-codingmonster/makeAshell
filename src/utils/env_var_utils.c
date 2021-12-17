8205a4d539794a602c3b1f9a9954192b655742b8
condingmonster <namgyupark@NAMGYUs-MacBook-Air.local>
2021-12-17 21:26:57 +0900

    add env

--- src/utils/env_var_utils.c
+++ src/utils/env_var_utils.c
@@ -6,26 +6,97 @@
 /*   By: napark <napark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 16:18:25 by napark            #+#    #+#             */
-/*   Updated: 2021/12/15 00:46:31 by napark           ###   ########.fr       */
+/*   Updated: 2021/12/17 21:26:24 by napark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "env_var_utils.h"
 
-t_env   *init_envv(char **envp)
+static void	get_env_var_value_utils(t_env *envv, char *var, char **value, int i)
 {
-    t_env   *envv;
-    
-    envv = ft_calloc(1, sizeof(t_env));//홤경변수 초기화 메모리 할당
-    if (envv != NULL && envp != NULL && *envp != NULL)
-    {
-        envv->envp = envp;
-        envv->env_var = ft_str_arr_dup(envp);
-        envv->pwd = get_env_var_value(envv, "PWD");
+	if (ft_strlen(*value) == 0 && ft_strcmp(var, "PWD") == 0)
+	{
+		if (ft_strlen(envv->pwd) == 0)
+			*value = ft_calloc(1, sizeof (char));
+		else
+			*value = ft_strdup(envv->pwd);
+	}
+	else if (ft_strlen(*value) == 0 && ft_strcmp(var, "OLDPWD") == 0)
+	{
+		if (ft_strlen(envv->oldpwd) == 0)
+			*value = ft_calloc(1, sizeof (char));
+		else
+			*value = ft_strdup(envv->oldpwd);
+	}
+	else if (envv->env_var[i] == NULL && *value == NULL)
+		*value = ft_calloc(1, sizeof(char));
+}
+
+char	*get_env_var_value(t_env *envv, char *var)
+{
+	int		i;
+	char	*value;
+
+	value = NULL;
+	i = 0;
+	while (envv && envv->env_var && envv->env_var[i] != NULL)
+	{
+		if (ft_strcmp(var, envv->env_var[i]) == -61)
+		{
+			value = ft_strchr(envv->env_var[i++], '=') + 1;
+			if (ft_strlen(value) == 0)
+				value = ft_calloc(1, sizeof (char));
+			else
+				value = ft_strdup(value);
+			break ;
+		}
+		else if (ft_strcmp(var, envv->env_var[i++]) == 0)
+		{
+			value = ft_calloc(1, sizeof (char));
+			break ;
+		}
+	}
+	get_env_var_value_utils(envv, var, &value, i);
+	return (value);
+}
+
+t_env	*init_envv(char **envp)
+{
+	t_env	*envv;
+
+	envv = ft_calloc(1, sizeof(t_env));
+	if (envv != NULL && envp != NULL && *envp != NULL)
+	{
+		envv->envp = envp;
+		envv->env_var = ft_str_arr_dup(envp);
+		envv->pwd = get_env_var_value(envv, "PWD");
 		envv->oldpwd = get_env_var_value(envv, "OLDPWD");
 		if (envv->env_var != NULL && envv->pwd != NULL && envv->oldpwd != NULL)
 			return (envv);
-    }
-    return (NULL);
+	}
+	return (NULL);
+}
+
+void	free_envv(t_env *envv)
+{
+	envv->envp = NULL;
+	ft_free_str_array(&envv->env_var);
+	ft_free_str(&envv->pwd);
+	ft_free_str(&envv->oldpwd);
+	free(envv);
+}
+
+int	reinit_env_var(t_env *envv, char **argv)
+{
+	if (*envv->env_var == NULL)
+	{
+		envv->env_var = ft_realloc_str_arr(envv->env_var, 2);
+		if (envv->env_var == NULL)
+			return (EXIT_FAILURE);
+		envv->env_var[0] = ft_strdup(argv[1]);
+		if (envv->env_var[0] == NULL)
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
